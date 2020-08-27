7e2aa715813bae09cde6b80c2daf92581dffdd0f
jay <jb7959@naver.com>
2020-08-27 17:39:03 +0900

    init route

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
+const PORT = 4000;
 
-app.listen(4000, () => console.log('localhost:4000'));
+app.listen(PORT, () => console.log(`localhost:${PORT}`));
+
+app.get("/", (req, res) => res.send("hello from get"));
+app.get("/profiles", (req, res) => res.send("you r on my profile"));
