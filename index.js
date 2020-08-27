63a3a89749f9a5344b8b0a2ad0579f35a9c509e3
jay <jb7959@naver.com>
2020-08-28 08:52:46 +0900

    nodemon 추가

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+import express from "express";
 const app = express();
 const PORT = 4000;
 
 app.listen(PORT, () => console.log(`localhost:${PORT}`));
 
-app.get("/", (req, res) => res.send("hello from get"));
+app.get("/", (req, res) => res.send("hello from get!!"));
 app.get("/profiles", (req, res) => res.send("you r on my profile"));
